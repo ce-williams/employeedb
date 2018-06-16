83e3ad302a69ab4667b96593afd44de671b0d9ba
cewilliams <cewilliams@csbsju.edu>
2018-06-16 11:16:32 -0500

    js

---
+++ app.js
@@ -0,0 +1,12 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDmpqUUMU_T0w-rSzfTz6Lo2PwTi5fJ10o",
+    authDomain: "employee-db-5b83f.firebaseapp.com",
+    databaseURL: "https://employee-db-5b83f.firebaseio.com",
+    projectId: "employee-db-5b83f",
+    storageBucket: "employee-db-5b83f.appspot.com",
+    messagingSenderId: "232525928558"
+  };
+  firebase.initializeApp(config);
+
+
